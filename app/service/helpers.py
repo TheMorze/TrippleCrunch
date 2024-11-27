8ebd3ccbfbf8a2489f233765d5993bbfef6d9885
Morze <kronto70@gmail.com>
2024-11-27 10:54:35 +0300

    Extra code deleted

--- app/service/helpers.py
+++ app/service/helpers.py
@@ -1,7 +1,1 @@
-async def define_author_name(username, show_author):
-    if show_author:
-        author_name = f'@{username}'
-    else:
-        author_name = '<i>Анонимно</i>'
-        
-    return author_name
+pass
