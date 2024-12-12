ac7603bf70ebda0a798d0d8720646e2fd142f1d7
Morze <kronto70@gmail.com>
2024-12-12 20:30:44 +0300

    changes #0006

--- app/service/helpers.py
+++ app/service/helpers.py
@@ -13,29 +13,6 @@ async def get_gpt_response(prompt):
 
     return response.choices[0].message.content
 
-
-# from g4f.client import AsyncClient
-# from g4f.Provider import OpenaiChat, Gemini
-
-# async def get_gpt_response_2(prompt: str):
-#     client = AsyncClient(
-#         provider=OpenaiChat,
-#         image_provider=Gemini,
-#         # Add other parameters as needed
-#     )
-
-#     response = await client.chat.completions.create(
-#     model="gpt-4o-mini",
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": prompt
-#         }
-#     ]
-#      # Add other parameters as needed
-#     )
-
-#     return response.choices[0].message.content
 async def get_llama_response(prompt):
     client = AsyncOpenAI(api_key=os.environ.get("LLAMA_API_KEY"), base_url=os.environ.get("LLAMA_BASE_URL"))
     response = await client.chat.completions.create(
